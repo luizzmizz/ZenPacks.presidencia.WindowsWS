47f65ee16e165a9ae4552b0d37605e2e4bf55865
luizzmizz <luizzmizz@gmail.com>
2012-04-19 16:57:42 +0200

    Using 1 du per path instead of 2 (du $path works!)

--- ZenPacks/presidencia/WindowsWS/libexec/getProfileSize.sh
+++ ZenPacks/presidencia/WindowsWS/libexec/getProfileSize.sh
@@ -8,20 +8,8 @@ unitat=`echo $PROFILEPATH|awk -F ":" '{print $1}'`
 path=`echo $PROFILEPATH|awk -F ":" '{print $2}'`
 
 if ping -c 1 $COMPUTER > /dev/null ;then
- if  ! smbclient //$COMPUTER/$unitat$ -U $USERNAME $PASSWORD -c="cd \"$path\"" 2>/dev/null |grep -q "OBJECT_NAME_NOT_FOUND" ;then
- #  echo "Profile Path exists: calculating..." 
-   profileSize=`smbclient //$COMPUTER/$unitat$ -U $USERNAME $PASSWORD -c="recurse;cd \"$path\";du" 2>/dev/null|grep "Total number of bytes:"|awk -F ": " '{print $2}'`
- else 
-   profileSize=-1
- fi
- 
- if  ! smbclient //$COMPUTER/$unitat$ -U $USERNAME $PASSWORD -c="cd \"$path\"/Desktop" 2>/dev/null |grep -q "OBJECT_NAME_NOT_FOUND" ;then 
- #  echo "Profile Desktop exists:calculating...."
-   desktopSize=`smbclient //$COMPUTER/$unitat$ -U $USERNAME $PASSWORD -c="recurse;cd \"$path\"/Desktop;du" 2>/dev/null|grep "Total number of bytes:"|awk -F ": " '{print $2}'`
- else 
-   desktopSize=-1
- fi
- 
- echo -n "profileSize:$profileSize "
- echo -n "desktopSize:$desktopSize "
+   profileSize=`smbclient //$COMPUTER/$unitat$ -U $USERNAME $PASSWORD -c="recurse;du \"$path\"" 2>/dev/null|grep "Total number of bytes:"|awk -F ": " '{print $2}'`
+   desktopSize=`smbclient //$COMPUTER/$unitat$ -U $USERNAME $PASSWORD -c="recurse;du \"$path\"/Desktop" 2>/dev/null|grep "Total number of bytes:"|awk -F ": " '{print $2}'`
+   echo -n "profileSize:$profileSize "
+   echo -n "desktopSize:$desktopSize "
 fi
